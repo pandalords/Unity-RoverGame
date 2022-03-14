452e0bcaa8a1dcb666adc851da3027bf24a74ea5
BlobbyCHEZ <dwightgriffith7@gmail.com>
2022-03-14 14:20:11 +1100

    Added Level Select Menu

--- Rover/Assets/Scripts/MainMenu.cs
+++ Rover/Assets/Scripts/MainMenu.cs
@@ -15,4 +15,29 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void LoadLevelOne()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadLevelTwo()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadLevelThree()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void LoadLevelFour()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void LoadLevelFive()
+    {
+        SceneManager.LoadScene(5);
+    }
 }
